bc444b84cb2b6c0e5b6bdd95e1b8a9033ad7e2b2
Marcin Adamowski <marcin.adamowski@gmail.com>
2013-07-08 23:38:37 +0100

    Read text from file

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+buf = new Buffer(30);
+
+var fs = require('fs');
+fs.readFile('index.html', function (err, data) {
+  buf.write(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
